4d51c1dda86bcea1e29bb8bce189914151a429d1
venegu <venegu@gmail.com>
2016-03-22 22:10:41 -0400

    Adding basic empty project

---
+++ tappyKitteh/GameScene.swift
@@ -0,0 +1,26 @@
+//
+//  GameScene.swift
+//  tappyKitteh
+//
+//  Created by Gale on 3/22/16.
+//  Copyright (c) 2016 Gale. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+        
+       
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
